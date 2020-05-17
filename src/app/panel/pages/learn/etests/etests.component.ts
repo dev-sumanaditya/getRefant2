78885327d64c974890e262e6cbf896c4d47bfea0
Aditya Suman <sumanaditya19@gmail.com>
2020-05-17 12:20:06 +0530

    tests page added

--- src/app/panel/pages/learn/etests/etests.component.ts
+++ src/app/panel/pages/learn/etests/etests.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EtestsComponent implements OnInit {
 
+  public trending = [1,1,1,1,1,1,1];
+
+  public subjects = [
+    {
+      name: 'Language',
+      id: 1
+    },
+    {
+      name: 'Computers',
+      id: 2
+    },
+    {
+      name: 'Physics',
+      id: 3
+    },
+    {
+      name: 'Chemistry',
+      id: 4
+    },
+    {
+      name: 'Mathematics',
+      id: 5
+    }
+  ];
+  selected = 2;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectSubject(id) {
+    this.selected = id;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { AvatarComponent } from './avatar/avatar.component';
 import { BlogcardComponent } from './blogcard/blogcard.component';
 import { BookComponent } from './book/book.component';
+import { TestcardComponent } from './testcard/testcard.component';
 
 
 
 @NgModule({
-  declarations: [AvatarComponent, BlogcardComponent, BookComponent],
+  declarations: [AvatarComponent, BlogcardComponent, BookComponent, TestcardComponent],
   imports: [
     CommonModule
   ],
   exports: [
     AvatarComponent,
     BlogcardComponent,
-    BookComponent
+    BookComponent,
+    TestcardComponent
   ]
 })
 export class SharedModule { }
